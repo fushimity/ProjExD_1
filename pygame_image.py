c949759a9f54ab6f6f0e3cd45dbad864ab903f4a
fushimity <fushimity@edu.teu.ac.jp>
2024-06-11 16:19:34 +0900

    練習６：背景画像の間延び対策

--- pygame_image.py
+++ pygame_image.py
@@ -17,12 +17,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        x = tmr%800
+        screen.blit(bg_img, [-x, 0])
         kk_rct = kk_img.get_rect()  # こうかとんRectの抽出
         kk_rct.center = 300, 200
         screen.blit(kk_img, kk_rct)  # kk_imgをkk_rctの設定にしたがって貼り付け
         pg.display.update()
-        tmr += 1        
+        tmr += 1      
         clock.tick(200)
 
 
