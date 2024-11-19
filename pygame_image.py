938c4c0cb8f86a4deb4f846be5db472922b2eb2c
fushimity <fushimity@edu.teu.ac.jp>
2024-11-19 15:55:48 +0900

    練習６：背景画像が流れるようにする

--- pygame_image.py
+++ pygame_image.py
@@ -16,8 +16,8 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])  # screen Surfaceに背景画像Surfaceを貼り付ける
+        x = tmr%800  # 練習6-2
+        screen.blit(bg_img, [-x, 0])  # screen Surfaceに背景画像Surfaceを貼り付ける
         screen.blit(kk_img, [300, 200])  # screen Surfaceにこうかとん画像Surfaceを貼り付ける
         pg.display.update()
         tmr += 1        
