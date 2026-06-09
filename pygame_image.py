7ed2b7e9a8bf534cafab8eaf2acfb37b3771fb8b
fushimity <fushimity@edu.teu.ac.jp>
2026-06-09 15:34:19 +0900

    練習3と4：こうかとん画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")  # 練習3：こうかとんSurfaceの作成
+    kk_img = pg.transform.flip(kk_img, True, False)  # 練習3：こうかとんの左右反転
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [300, 200])  # 練習4：こうかとんSurfaceの描画
         pg.display.update()
         tmr += 1        
         clock.tick(10)
